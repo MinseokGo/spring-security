bb40198d8b5d8760ef5d364fe0b3d790c7c4d99b
MinseokGo <rhalstjr1999@naver.com>
2024-04-25 14:34:08 +0900

    feat: get token at google login

--- src/main/java/study/openfeign/legacy/LegacyController.java
+++ src/main/java/study/openfeign/legacy/LegacyController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,22 +12,32 @@ import study.openfeign.legacy.utils.LegacyUtils;
 
 @Slf4j
 @Controller
-@RequestMapping("/login")
 @RequiredArgsConstructor
+@RequestMapping("/login")
 public class LegacyController {
 
     private final LegacyUtils legacyUtils;
     private final LegacyService legacyService;
 
     @GetMapping("/kakao")
-    public String call() {
+    public String callKakaoLogin() {
         return legacyUtils.uriCreate();
     }
 
     @ResponseBody
     @GetMapping("/kakao/redirect")
-    public String redirect(@RequestParam("code") String code) {
+    public String redirectKakao(@RequestParam("code") String code) {
         legacyService.create(code);
         return "good";
+    }
+
+    @GetMapping("/google")
+    public String callGoogleLogin() {
+        return legacyUtils.googleUri();
+    }
+
+    @GetMapping("/google/redirect")
+    public void redirectGoogle(@RequestParam("code") String code) {
+        System.out.println(code);
     }
 }
--- src/main/java/study/openfeign/legacy/utils/LegacyUtils.java
+++ src/main/java/study/openfeign/legacy/utils/LegacyUtils.java
@@ -9,10 +9,19 @@ public class LegacyUtils {
 
     private final AuthProperties authProperties;
 
+
     public String uriCreate() {
         return "redirect:https://kauth.kakao.com/oauth/authorize"
                 + "?response_type=code"
                 + "&client_id=" + authProperties.getClientId()
                 + "&redirect_uri=" + authProperties.getRedirectUri();
+    }
+
+    public String googleUri() {
+        return "redirect:https://accounts.google.com/o/oauth2/v2/auth"
+                + "?response_type=code"
+                + "&client_id=" + "902539090164-4h10hioh6e01p1eoo1lur7ut9bjlhj6v.apps.googleusercontent.com"
+                + "&redirect_uri=" + "http://localhost:8080/login/google/redirect"
+                + "&scope=email profile";
     }
 }
