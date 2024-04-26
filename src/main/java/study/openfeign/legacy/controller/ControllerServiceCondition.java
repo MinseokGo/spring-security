6712538362ccf788827d577a0cacf5509ce62405
MinseokGo <rhalstjr1999@naver.com>
2024-04-27 02:52:42 +0900

    refactor: controller service condition

--- src/main/java/study/openfeign/legacy/controller/ControllerServiceCondition.java
+++ src/main/java/study/openfeign/legacy/controller/ControllerServiceCondition.java
@@ -2,11 +2,9 @@ package study.openfeign.legacy.controller;
 
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import study.openfeign.legacy.controller.controllerservice.ControllerService;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ControllerServiceCondition {
@@ -14,7 +12,6 @@ public class ControllerServiceCondition {
     private final Map<String, ControllerService> controllerServiceMap;
 
     public ControllerService getControllerService(String name) {
-        controllerServiceMap.keySet().stream().forEach(System.out::println);
         return controllerServiceMap.get(name);
     }
 }
